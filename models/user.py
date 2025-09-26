8258230f66b7958e806280d732af2c7b4fa5d463
hector <h.rosales01@ufromail.cl>
2025-09-26 01:30:02 -0300

    feat: fields in class

--- models/user.py
+++ models/user.py
@@ -4,4 +4,8 @@ class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     email: str = Field(index=True, unique=True)
     full_name: str | None = Field(default=None)
+    nickname: str | None = Field(default=None, index=True)
+    role: str = Field(default="user")
+    phone_number: str | None = Field(default=None)
+    is_active: bool = Field(default=True)
     hashed_password: str
