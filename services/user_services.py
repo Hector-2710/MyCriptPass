52b68314b54502bdab5e813be53d863b2eb1ac2e
hector <h.rosales01@ufromail.cl>
2025-10-03 00:48:23 -0300

    refactor: remove class exception and verify unique nikname

--- services/user_services.py
+++ services/user_services.py
@@ -1,22 +1,21 @@
 from sqlmodel import Session, select
 from models.user import User
-from schemas.user import UserCreate, UserResponse
+from schemas.user import UserCreate
 from core.security import get_password_hash
 from typing import Optional
-
-class UserAlreadyExistsError(Exception):
-    def __init__(self, email: str):
-        self.email = email
-        super().__init__(f"User with email {email} already exists")
+from exceptions.exceptions import UserAlreadyExistsError, NicknameAlreadyExistsError
 
 def create_user(session: Session, user_data: UserCreate) -> User:
-    
     existing_user = get_user_by_email(session, user_data.email)
+    existing_nickname = get_user_by_nickname(session, user_data.nickname)
+
     if existing_user:
         raise UserAlreadyExistsError(email=user_data.email)
     
+    if existing_nickname:
+        raise NicknameAlreadyExistsError(nickname=user_data.nickname)
+
     hashed_password = get_password_hash(user_data.password)
-    
     user = User(
         email=user_data.email,
         full_name=user_data.full_name,
@@ -24,16 +23,15 @@ def create_user(session: Session, user_data: UserCreate) -> User:
         phone_number=user_data.phone_number,
         hashed_password=hashed_password
     )
-    
     session.add(user)
     session.commit()
     session.refresh(user)
-    
     return user
 
 def get_user_by_email(session: Session, email: str) -> Optional[User]:
     statement = select(User).where(User.email == email)
     return session.exec(statement).first()
 
-def get_user_by_id(session: Session, user_id: int) -> Optional[User]:
-    return session.get(User, user_id)
+def get_user_by_nickname(session: Session, nickname: str) -> Optional[User]:
+    statement = select(User).where(User.nickname == nickname)
+    return session.exec(statement).first()
