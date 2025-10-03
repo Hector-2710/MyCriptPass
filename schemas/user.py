81e503347dc4d954cf4384552717f79c6eb7a2a0
hector <h.rosales01@ufromail.cl>
2025-10-03 00:52:21 -0300

    refactor: remove class UserResponse and add params in Field for dooc

--- schemas/user.py
+++ schemas/user.py
@@ -1,22 +1,12 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr,Field
 from typing import Optional
 
 class UserBase(BaseModel):
-    email: EmailStr
-    full_name: Optional[str] = None
-    nickname: Optional[str] = None
-    phone_number: Optional[str] = None
+    email: EmailStr =  Field(...,description="The email of the user")
+    full_name: str = Field(..., description="The full name of the user", min_length=10, max_length=70)
+    nickname: str = Field(..., description="The nickname of the user", min_length=3, max_length=13)
+    phone_number: Optional[str] = Field(default=None, description="The phone number of the user", min_length=8, max_length=15)
 
 class UserCreate(UserBase):
-    password: str
-
-class UserUpdate(UserBase):
-    password: Optional[str] = None
-
-class UserResponse(UserBase):
-    id: int
-    role: str
-    is_active: bool
+    password: str = Field(..., min_length=8, description="Password must be at least 8 characters")
     
-    class Config:
-        from_attributes = True
