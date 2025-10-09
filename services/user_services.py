2b9eda3666a5085f4420d513b60edce26c630fa2
hector <h.rosales01@ufromail.cl>
2025-10-08 23:22:02 -0300

    refactor: realize create_user in db whit mongo

--- services/user_services.py
+++ services/user_services.py
@@ -1,37 +1,37 @@
-from sqlmodel import Session, select
 from models.user import User
 from schemas.user import UserCreate
 from core.security import get_password_hash
+from db.session import get_database
 from typing import Optional
 from exceptions.exceptions import UserAlreadyExistsError, NicknameAlreadyExistsError
 
-def create_user(session: Session, user_data: UserCreate) -> User:
-    existing_user = get_user_by_email(session, user_data.email)
-    existing_nickname = get_user_by_nickname(session, user_data.nickname)
+db = get_database()
+
+async def create_user(user_data: UserCreate) -> User:
+    existing_user = await get_user_by_email(user_data.email)
+    existing_nickname = await get_user_by_nickname(user_data.nickname)
 
     if existing_user:
         raise UserAlreadyExistsError(email=user_data.email)
-    
     if existing_nickname:
         raise NicknameAlreadyExistsError(nickname=user_data.nickname)
 
     hashed_password = get_password_hash(user_data.password)
-    user = User(
-        email=user_data.email,
-        full_name=user_data.full_name,
-        nickname=user_data.nickname,
-        phone_number=user_data.phone_number,
-        hashed_password=hashed_password
-    )
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    return user
+    user_dict = user_data.model_dump()
+    user_dict['hashed_password'] = hashed_password
+    user_dict.pop('password', None)  
 
-def get_user_by_email(session: Session, email: str) -> Optional[User]:
-    statement = select(User).where(User.email == email)
-    return session.exec(statement).first()
+    await db["users"].insert_one(user_dict)
+    return User(**user_dict)
 
-def get_user_by_nickname(session: Session, nickname: str) -> Optional[User]:
-    statement = select(User).where(User.nickname == nickname)
-    return session.exec(statement).first()
+async def get_user_by_email(email: str) -> Optional[User]:
+    user_data = await db["users"].find_one({"email": email})
+    if user_data:
+        return User(**user_data)
+    return None
+
+async def get_user_by_nickname(nickname: str) -> Optional[User]:
+    user_data = await db["users"].find_one({"nickname": nickname})
+    if user_data:
+        return User(**user_data)
+    return None
