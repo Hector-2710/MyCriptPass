5f7f1f961341059dbffa4fc9405b26637e2df2ad
hector <h.rosales01@ufromail.cl>
2025-10-08 23:17:03 -0300

    refactor: change from sql to mongodb

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
 from api.v1.routes import user
-from db.session import engine
-from sqlmodel import SQLModel
-from contextlib import asynccontextmanager
-from models.user import User
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    SQLModel.metadata.create_all(engine)  
-    yield
 
-app = FastAPI(title="MyCryptPass API", lifespan=lifespan)
-
+app = FastAPI(title="MyCryptPass API")
 app.include_router(user.app)
