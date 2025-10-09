44130db4d5622bf0ff4961b916445eda17c9e0e3
hector <h.rosales01@ufromail.cl>
2025-10-08 23:20:03 -0300

    refactor: model for document of the user

--- models/user.py
+++ models/user.py
@@ -1,10 +1,8 @@
-from sqlmodel import SQLModel, Field
-from pydantic import EmailStr
+from pydantic import BaseModel,EmailStr
 
-class User(SQLModel, table=True):
-    id: int | None = Field(primary_key=True,unique= True)  
-    email: EmailStr = Field(unique=True)
+class User(BaseModel):
     full_name: str 
-    nickname: str = Field(unique=True)
-    phone_number: str 
+    email: EmailStr 
+    nickname: str 
     hashed_password: str
+
