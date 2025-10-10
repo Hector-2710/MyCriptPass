51a41ae31ba159ad03a80bfa2bfe98280f2d68be
hector <h.rosales01@ufromail.cl>
2025-10-09 23:13:26 -0300

    feat: method for create and close connection for mongo

--- db/session.py
+++ db/session.py
@@ -9,3 +9,13 @@ db = client[DATABASE_NAME]
 
 def get_database():
     return db
+
+async def connect_to_mongo():
+    global client, db
+    client = AsyncIOMotorClient(settings.DATABASE_URL)
+    db = client[settings.DATABASE_NAME]
+
+async def close_mongo_connection():
+    global client
+    if client:
+        client.close()
