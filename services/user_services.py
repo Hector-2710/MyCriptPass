81a8000ecb776634aa4cee73d57b490658537e20
hector <h.rosales01@ufromail.cl>
2025-10-09 23:18:24 -0300

    refactor: methods add "db" as  a param

--- services/user_services.py
+++ services/user_services.py
@@ -1,15 +1,12 @@
 from models.user import User
 from schemas.user import UserCreate
 from core.security import get_password_hash
-from db.session import get_database
-from typing import Optional
+from typing import Optional, Any
 from exceptions.exceptions import UserAlreadyExistsError, NicknameAlreadyExistsError
 
-db = get_database()
-
-async def create_user(user_data: UserCreate) -> User:
-    existing_user = await get_user_by_email(user_data.email)
-    existing_nickname = await get_user_by_nickname(user_data.nickname)
+async def create_user(user_data: UserCreate, db=Any) -> User:
+    existing_user = await get_user_by_email(user_data.email, db)
+    existing_nickname = await get_user_by_nickname(user_data.nickname, db)
 
     if existing_user:
         raise UserAlreadyExistsError(email=user_data.email)
@@ -24,13 +21,13 @@ async def create_user(user_data: UserCreate) -> User:
     await db["users"].insert_one(user_dict)
     return User(**user_dict)
 
-async def get_user_by_email(email: str) -> Optional[User]:
+async def get_user_by_email(email: str, db=Any) -> Optional[User]:
     user_data = await db["users"].find_one({"email": email})
     if user_data:
         return User(**user_data)
     return None
 
-async def get_user_by_nickname(nickname: str) -> Optional[User]:
+async def get_user_by_nickname(nickname: str, db=Any) -> Optional[User]:
     user_data = await db["users"].find_one({"nickname": nickname})
     if user_data:
         return User(**user_data)
