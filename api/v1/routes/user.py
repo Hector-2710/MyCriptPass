bc98ddb1433d89746bec82b2e888a0197674b970
hector <h.rosales01@ufromail.cl>
2025-10-09 23:12:32 -0300

    refactor: the manage of exceptions another class does it

--- api/v1/routes/user.py
+++ api/v1/routes/user.py
@@ -1,30 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,status
 from schemas.user import UserCreate
 from models.user import User
 from services.user_services import create_user
-from fastapi import HTTPException, status
-from exceptions.exceptions import UserAlreadyExistsError, NicknameAlreadyExistsError
+from db.session import get_database
 
-app = APIRouter(prefix="/user", tags=["user"])
+router = APIRouter(prefix="/user", tags=["user"])
 
-@app.post("/create", response_model=User,response_model_exclude={"hashed_password"}, status_code=status.HTTP_201_CREATED)
-async def create_user_endpoint(user_data: UserCreate) -> User:
-    try:
-        user = await create_user(user_data)  
-        return user
-    
-    except UserAlreadyExistsError as e:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail= str(e)
-        )
-    except NicknameAlreadyExistsError as e:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail=str(e)
-        )
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Internal server error"
-        )
+@router.post("", response_model=User,response_model_exclude={"hashed_password"}, status_code=status.HTTP_201_CREATED)
+async def create_user_endpoint(user_data: UserCreate, db=Depends(get_database)) -> User:
+    user = await create_user(user_data, db)  
+    return user
+ 
+  
