763483844a245dbc2e55576422f061b9b4772b31
hector <h.rosales01@ufromail.cl>
2025-10-09 23:14:33 -0300

    refactpr: Exceptions inherit of the same class "AppBaseError"

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,9 +1,16 @@
-class UserAlreadyExistsError(Exception):
+class AppBaseError(Exception):
+    status_code: int = 400  
+
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
+
+class UserAlreadyExistsError(AppBaseError):
     def __init__(self, email: str):
         self.email = email
         super().__init__(f"User with email {email} already exists")
 
-class NicknameAlreadyExistsError(Exception):
+class NicknameAlreadyExistsError(AppBaseError):
     def __init__(self, nickname: str):
         self.nickname = nickname
         super().__init__(f"User with nickname {nickname} already exists")
