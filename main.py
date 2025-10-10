315d2267ca09a4110e260a45317d156019ab5e42
hector <h.rosales01@ufromail.cl>
2025-10-09 23:09:46 -0300

    feat: add lisfespan for manage connections in mongo

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from api.v1.routes import user
+from exceptions.handlers import register_exception_handlers
+from db.session import connect_to_mongo, close_mongo_connection
+from contextlib import asynccontextmanager
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await connect_to_mongo()
+    yield
+    await close_mongo_connection()
 
-app = FastAPI(title="MyCryptPass API")
-app.include_router(user.app)
+app = FastAPI(title="MyCryptPass API", version="1.0.0", lifespan=lifespan,description="API for managing user passwords securely")
+app.include_router(user.router)
+register_exception_handlers(app)
+
