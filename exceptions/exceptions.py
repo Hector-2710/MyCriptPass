46cfea7ca9c310b5bdb1b6b39c2d217f5604901b
hector <h.rosales01@ufromail.cl>
2025-10-21 22:59:10 -0300

    feat: standardize error handling by updating message parameter to detail in exception classes

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,17 +1,31 @@
 from fastapi import HTTPException
 
+from pydantic import BaseModel
+
+class ErrorResponse(BaseModel):
+    detail: str
+
+
 class AppBaseError(HTTPException):
-    def __init__(self, status_code: int, message: str):
-        super().__init__(status_code=status_code, detail=message)
+    def __init__(self, status_code: int, detail: str):
+        super().__init__(status_code=status_code, detail=detail)
         
 class UserAlreadyExistsError(AppBaseError):
     def __init__(self, email: str):
-        super().__init__(status_code=409, message=f"User with email {email} already exists")
+        super().__init__(status_code=409, detail=f"User with email {email} already exists")
 
 class NicknameAlreadyExistsError(AppBaseError):
     def __init__(self, nickname: str):
-        super().__init__(status_code=409, message=f"User with nickname {nickname} already exists")
+        super().__init__(status_code=499, detail=f"User with nickname {nickname} already exists")
 
 class UserNotFoundError(AppBaseError):
     def __init__(self, email: str):
-        super().__init__(status_code=404, message=f"User with email {email} not found")
+        super().__init__(status_code=404, detail=f"User with email {email} not found")
+
+class UnauthorizedError(AppBaseError):
+    def __init__(self, user: str):
+        super().__init__(status_code=401, detail=f"You are not authorized to delete user {user}")
+
+class IncorrectPasswordError(AppBaseError):
+    def __init__(self, nickname: str):
+        super().__init__(status_code=401, detail=f"Incorrect password for nickname {nickname}")
