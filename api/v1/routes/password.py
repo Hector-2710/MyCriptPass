ddbf8297058e0cf75c7d076ccac6a63f0d4fd5be
hector <h.rosales01@ufromail.cl>
2025-11-06 19:41:02 -0300

    feat: endpoint for get password by service name

--- api/v1/routes/password.py
+++ api/v1/routes/password.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from schemas.password import PasswordCreate, PasswordResponse
-from services.password_services import create_password
+from services.password_services import create_password, get_password
 from db.session import get_database
 from fastapi import Depends,status
 from core.security import get_current_user
@@ -19,3 +19,12 @@ async def create_password_endpoint(password_data: PasswordCreate, User = Depends
     password = await create_password(User.nickname, password_data, db)
     return password
 
+@router.get("/{service_name}", summary="Obtener una contraseña", response_description="Contraseña obtenida exitosamente", status_code=status.HTTP_200_OK)
+async def get_password_endpoint(service_name: str, User = Depends(get_current_user), db=Depends(get_database)) :
+    """
+    Obtiene una contraseña de la base de datos.
+    - **nickname**: nickname del usuario
+    - **service_name**: nombre del servicio
+    """
+    password = await get_password(User.nickname, service_name, db)
+    return password
