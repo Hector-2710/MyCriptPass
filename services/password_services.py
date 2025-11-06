19530b1ab06568c535b118824d730a12489d0ea4
hector <h.rosales01@ufromail.cl>
2025-11-06 19:42:01 -0300

    feat: get_password function get password for service name in db

--- services/password_services.py
+++ services/password_services.py
@@ -1,10 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from schemas.password import PasswordCreate, PasswordResponse
-from exceptions.exceptions import PasswordAlreadyExistsError
+from exceptions.exceptions import PasswordAlreadyExistsError, PasswordNotFoundError
 from core.security import get_password_hash
 
 async def create_password(nickname: str, password_data: PasswordCreate, db: AsyncIOMotorDatabase) -> PasswordResponse:
-    existing = await exists_password(nickname, password_data.service_name, db)
+    existing = await exists_password_for_service(nickname, password_data.service_name, db)
     if existing:
         raise PasswordAlreadyExistsError(nickname=nickname, service_name=password_data.service_name)
 
@@ -16,8 +16,20 @@ async def create_password(nickname: str, password_data: PasswordCreate, db: Asyn
     )
 
     return {"service_name": password_data.service_name}
- 
-async def exists_password(nickname: str, service_name: str, db: AsyncIOMotorDatabase) -> bool:
+
+
+async def get_password(nickname: str, service_name: str, db: AsyncIOMotorDatabase) -> PasswordResponse:
+    exists = await exists_password_for_service(nickname, service_name, db)
+    if not exists:
+        return PasswordNotFoundError(nickname=nickname, service_name=service_name)
+    password = await db["users"].find_one(
+        {"nickname": nickname, "passwords.app_service": service_name},
+        {"passwords.$": 1, "_id": 0},
+    )
+    print(password)
+    return password
+    
+async def exists_password_for_service(nickname: str, service_name: str, db: AsyncIOMotorDatabase) -> bool:
     doc = await db["users"].find_one(
         {"nickname": nickname, "passwords.app_service": service_name},
         {"_id": 1},
