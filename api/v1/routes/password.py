f30809a7cdf92082a04b370a388e85f7c4e3e497
hector <h.rosales01@ufromail.cl>
2025-12-16 16:18:57 -0300

    feat: add error handling for password retrieval and fix variable naming

--- api/v1/routes/password.py
+++ api/v1/routes/password.py
@@ -4,6 +4,7 @@ from services.password_services import create_password, get_password
 from db.session import get_database
 from fastapi import Depends,status
 from core.security import get_current_user
+from exceptions.exceptions import PasswordNotExists
 
 
 router = APIRouter(prefix="/passwords", tags=["passwords"])
@@ -19,12 +20,12 @@ async def create_password_endpoint(password_data: PasswordCreate, User = Depends
     password = await create_password(User.nickname, password_data, db)
     return password
 
-@router.get("/{service_name}", summary="Obtener una contraseña", response_description="Contraseña obtenida exitosamente", status_code=status.HTTP_200_OK)
-async def get_password_endpoint(service_name: str, User = Depends(get_current_user), db=Depends(get_database)) :
+@router.get("/{service_name}",summary="Obtener una contraseña",responses={404: {"model": PasswordNotExists, "description":"Contraseña no encontrada"}}, response_description="Contraseña obtenida exitosamente", status_code=status.HTTP_200_OK)
+async def get_password_endpoint(service_name: str, user = Depends(get_current_user), db=Depends(get_database)) :
     """
     Obtiene una contraseña de la base de datos.
     - **nickname**: nickname del usuario
     - **service_name**: nombre del servicio
     """
-    password = await get_password(User.nickname, service_name, db)
+    password = await get_password(user.nickname, service_name, db)
     return password
