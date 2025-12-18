85867971463d532b1654f2213a9f5aea20e6316f
hector <h.rosales01@ufromail.cl>
2025-12-18 15:01:26 -0300

    feat: update password endpoint for documentation

--- api/v1/routes/password.py
+++ api/v1/routes/password.py
@@ -4,13 +4,13 @@ from services.password_services import create_password, get_password, delete_pas
 from db.session import get_database
 from fastapi import Depends,status
 from core.security import get_current_user
-from exceptions.exceptions import PasswordNotFound
+from exceptions.exceptions import PasswordNotFound, PasswordAlreadyExists
 from schemas.password import GetPasswordResponse
 
 
-router = APIRouter(prefix="/passwords", tags=["passwords"])
+router = APIRouter(prefix="/password", tags=["passwords"])
 
-@router.post("/", summary="Crear una nueva contraseña",response_description="Contraseña creada exitosamente",response_model=PasswordResponse, status_code=status.HTTP_201_CREATED)
+@router.post("/", summary="Crear una nueva contraseña",response_description="Contraseña creada exitosamente",response_model=PasswordResponse,responses={409: {"model": PasswordAlreadyExists, "description":"Contraseña ya existe"}} ,status_code=status.HTTP_201_CREATED)
 async def create_password_endpoint(password_data: PasswordCreate, User = Depends(get_current_user), db=Depends(get_database)) -> PasswordResponse:
     """
     Crea una nueva contraseña en la base de datos.
